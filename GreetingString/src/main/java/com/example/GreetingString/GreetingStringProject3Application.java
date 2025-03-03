545b210306205f8bc2481e4ef1ec4c30e94d2027
DeveshhTripathi <deveshht591@gmail.com>
2025-03-03 14:30:58 +0530

    Devesh Tripathi addition of different methods Http methods

---
+++ GreetingString/src/main/java/com/example/GreetingString/GreetingStringProject3Application.java
@@ -0,0 +1,13 @@
+package com.example.GreetingString;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingStringProject3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingStringProject3Application.class, args);
+	}
+
+}
---
+++ GreetingString/src/test/java/com/example/GreetingString/GreetingStringProject3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GreetingString;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingStringProject3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
